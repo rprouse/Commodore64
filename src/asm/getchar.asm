90ef64f92d6f504235b6489473b7e47b987b07bd
Rob Prouse <rob@prouse.org>
2021-06-14 20:43:43 -0400

    Accept alphanumeric

--- src/asm/getchar.asm
+++ src/asm/getchar.asm
@@ -20,10 +20,16 @@ start:
   jsr STOP
   beq @end
   jsr GETIN
+  cmp #CHAR_0
+  bcc start
+  cmp #CHAR_9+1
+  bcc @out
   cmp #CHAR_A
   bcc start
   cmp #CHAR_Z+1
   bcs start
+
+@out:
   jsr CHROUT
   and #$0F
 
