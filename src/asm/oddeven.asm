0014a7fb4d689dec2fdf7a37774a95f1170e38ac
Rob Prouse <rob@prouse.org>
2021-06-16 22:05:58 -0400

    Determine if a number is odd or even

---
+++ src/asm/oddeven.asm
@@ -0,0 +1,52 @@
+.include "kernal.inc"
+.include "petscii.inc"
+
+start:
+  lda #CLRSCRN  ; Clear the screen
+  jsr CHROUT
+  jsr @getnum   ; Get the number
+  clc
+  lsr
+  bcs @odd
+  lda #' '
+  jsr CHROUT
+  lda #'e'
+  jsr CHROUT
+  lda #'v'
+  jsr CHROUT
+  lda #'e'
+  jsr CHROUT
+  lda #'n'
+  jsr CHROUT
+  jmp @end
+
+@odd:
+  lda #' '
+  jsr CHROUT
+  lda #'o'
+  jsr CHROUT
+  lda #'d'
+  jsr CHROUT
+  lda #'d'
+  jsr CHROUT
+  jmp @end
+
+@getnum:        ; Get 0 through 9 and return it on the A register
+  jsr STOP
+  beq @end
+  jsr GETIN
+  cmp #CHAR_0
+  bcc @getnum
+  cmp #CHAR_9+1
+  bcs @getnum
+  jsr CHROUT
+  and #$0F
+  rts
+
+@end:
+  lda #NEWLINE
+  jsr CHROUT
+  rts
+
+num:
+  .byte $00
