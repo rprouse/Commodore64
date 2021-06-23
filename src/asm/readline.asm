874510e08b30ecc302356f94734d20fbd7ab6855
Rob Prouse <rob@prouse.org>
2021-06-22 20:24:04 -0400

    Read a line, store it then echo it

--- src/asm/readline.asm
+++ src/asm/readline.asm
@@ -1,30 +1,37 @@
+; This program reads in up to one line of text, stores it in memory, then
+; prints the line back to the screen when the user its enter.
 .include "kernal.inc"
 .include "petscii.inc"
 
+ZP_X := $BB
+
 start:
   ldx #$0
+  stx ZP_X    ; Store the X register because GETIN clobbers it
 @input:
-  jsr STOP
+  jsr STOP    ; Check if run/stop key was pressed
   beq @end
-  jsr GETIN
+  jsr GETIN   ; This changes the A, X, Y registers
   cmp #$0
-  beq @input
-  sta line,x
-  jsr CHROUT
+  beq @input  ; Nothing was pressed
+  ldx ZP_X    ; Restore the X register
+  sta line,x  ; Save out the character to memory
+  jsr CHROUT  ; Echo the character to the screen
 
   inx
-  cmp #NEWLINE
+  stx ZP_X
+  cmp #NEWLINE  ; Did the user hit return?
   beq @out
-  cpx width
+  cpx width     ; Have we hit the end of the line?
   bcc @input
 
 @out:
-  ldx #$0
+  ldx #$0       ; Reset x
 @loop:
-  lda line,x
-  jsr CHROUT
+  lda line,x    ; Load the character and echo it
+  jsr CHROUT    ; Only changes the A register
   inx
-  cmp #NEWLINE
+  cmp #NEWLINE  ; Stop when we hit a newline
   bne @loop
 
 @end:
@@ -34,8 +41,7 @@ width:
   .byte $28
 
 line:
-  .byte "01234567890123456789012345678",NEWLINE
-  ; .res 12,NEWLINE ; Why is this limited to 12?
-  ; .res 12,NEWLINE
-  ; .res 12,NEWLINE
-  ; .res 4,NEWLINE
+  .res 12,NEWLINE ; Why is this limited to 12?
+  .res 12,NEWLINE
+  .res 12,NEWLINE
+  .res 4,NEWLINE
