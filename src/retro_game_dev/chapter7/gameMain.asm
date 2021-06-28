0c57e14e28d51e432c8a81a4c0a4f09792cc58de
Rob Prouse <rob@prouse.org>
2021-06-27 22:40:49 -0400

    Working on chapter 7

--- src/retro_game_dev/chapter7/gameMain.asm
+++ src/retro_game_dev/chapter7/gameMain.asm
@@ -2,6 +2,8 @@
 .include "../lib/libMemory.inc"
 .include "../lib/libMath.inc"
 .include "../lib/libScreen.inc"
+.include "../lib/libSprite.inc"
+.include "sprites.inc"
 
 ;===============================================================================
 ; Initialize
@@ -9,28 +11,31 @@
 .segment "CODE"
 
 gameMainInit:
-        ; Turn off interrupts to stop LIBSCREEN_WAIT failing every so
-        ; often when the kernal interrupt syncs up with the scanline test
-        sei
+  ; Turn off interrupts to stop LIBSCREEN_WAIT failing every so
+  ; often when the kernal interrupt syncs up with the scanline test
+  sei
 
-        ; Switch to lowercase mode
-        lda $17 ; UPPERCASE MODE is $25
-        sta VMCSB
+  ; Switch to lowercase mode
+  lda $17 ; UPPERCASE MODE is $25
+  sta VMCSB
 
-        ; Disable run/stop + restore keys
-        lda #$FC
-        sta $0328
+  ; Disable run/stop + restore keys
+  lda #$FC
+  sta $0328
 
-        ; Set border and background colors
-        ; The last 3 parameters are not used yet
-        LIBSCREEN_SETCOLORS Blue, White, Black, Black, Black
+  ; Set border and background colors
+  ; The last 3 parameters are not used yet
+  LIBSCREEN_SETCOLORS Black, Black, Black, Black, Black
+
+  ; Set the sprite multicolors
+  LIBSPRITE_SETMULTICOLORS_VV Black, Black
 
 ;===============================================================================
 ; Update
 
 gMLoop:
-        LIBSCREEN_WAIT_V 255
-        ;inc EXTCOL ; start code timer change border color
-        ; Game update code goes here
-        ;dec EXTCOL ; end code timer reset border color
-        jmp gMLoop
+  LIBSCREEN_WAIT_V 255
+  ;inc EXTCOL ; start code timer change border color
+  ; Game update code goes here
+  ;dec EXTCOL ; end code timer reset border color
+  jmp gMLoop
