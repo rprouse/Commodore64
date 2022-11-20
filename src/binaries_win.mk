92ac10fe536e191c340008ada5cd7ebe49ff7f0c
Rob Prouse <rob@prouse.org>
2022-11-20 17:33:21 -0500

    Fixing the make files to work on new computer

--- src/binaries_win.mk
+++ src/binaries_win.mk
@@ -1,5 +1,5 @@
-# Windows specific binaries
-RM_BINARY=rmdir
-RM_FLAGS=/S /Q
-MKDIR_BINARY=mkdir
-MKDIR_FLAGS=
+# Windows specific binaries, assumes that GNU tools are installed with Git
+RM_BINARY=rm.exe
+RM_FLAGS=-rf
+MKDIR_BINARY=mkdir.exe
+MKDIR_FLAGS=-p
